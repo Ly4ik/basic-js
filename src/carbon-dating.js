bc48ce898f44bad213829c74d534f96c216b576d
Pavel Ryndzenka <Pavelryndzenko@gmail.com>
2023-12-21 15:51:51 +0300

    feat: complete count-cats task

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -25,7 +25,6 @@ function dateSample(sampleActivity) {
   }
   const k = 0.693 / HALF_LIFE_PERIOD;
   const t = Math.ceil((Math.log(MODERN_ACTIVITY / sampleActivity) / k));
-  console.log(type == 'number');
   return t;
 }
 
